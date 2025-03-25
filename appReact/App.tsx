f3773b26d79fc9b052c0ef7f3478a8d0fb614e05
BrunoCesarioMenezes <brunoagjf@gmail.com>
2025-03-25 20:16:18 -0300

    estiliza statusbar tema claro

--- appReact/App.tsx
+++ appReact/App.tsx
@@ -1,22 +1,11 @@
-import { View, Text, StyleSheet } from "react-native";
+import { Home } from './src/screens/Home';
+import { StatusBar } from 'react-native';
 
 export default function App(){
     return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Olá, mundo!</Text>
-    </View>
+      <>
+        <StatusBar barStyle="light-content" translucent />
+        <Home/>
+      </>
     )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor:"rgb(17, 16, 16)",
-    padding: 24
-  },
-  text : {
-    color: "white",
-    fontWeight: 'bold',
-    fontSize: 24,
-  }
-})
